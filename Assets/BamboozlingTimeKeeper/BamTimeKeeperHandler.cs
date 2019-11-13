b29506c9d9eb8e4d7fa9b72ecb4c0638a27a2e85
Vincent Flyer (Vincent Chan) <35939457+VFlyer@users.noreply.github.com>
2019-11-12 19:25:58 -0800

    Initial Commit (Start Project)

---
+++ Assets/BamboozlingTimeKeeper/BamTimeKeeperHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KModkit;
+public class BamTimeKeeperHandler : MonoBehaviour {
+
+    public KMBombInfo info;
+    public KMColorblindMode colorblindMode;
+    public KMAudio sound;
+    public GameObject buttonL, buttonM, buttonR, highlightStage1, hightlightStage2;
+    public GameObject animationPointDoorA, animationPointDoorB, animationPointButtonA, animationPointButtonB;
+    public TextMesh display;
+    public Material[] buttonColors = new Material[8];
+
+    public Material[] indicatorStatus = new Material[3];
+
+    private ulong finalValueA;
+    private ulong finalValueB;
+    private List<ulong> possibleTimesA = new List<ulong>();
+    private List<ulong> possibleTimesB = new List<ulong>();
+
+    // Use this for initialization
+    void Start () {
+        sound.PlaySoundAtTransform("KefkaLaugh", transform);
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+    IEnumerator HideButtons()
+    {
+        yield return null;
+    }
+}
